5e3ca4d8991245a600d4b0278c9cbf63d8cd7e91
Bohdan Partyk <info@podebrady.org>
2018-10-19 13:13:39 +0200

    removed unused libs from task copy

--- gulp/task/copy.js
+++ gulp/task/copy.js
@@ -5,10 +5,6 @@ const gulp = require('gulp');
 
 const copy = require('copy');
 
-const plugins = require('gulp-load-plugins');
-
-const $ = plugins();
-
 const streamCopy = (name, src, dist, callback) => {
     console.info(`${name} > copy of files`);
     copy(src, dist, callback);
