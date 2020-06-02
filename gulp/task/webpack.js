8a661ca3de0e339422b828d6f8bac71cacd2d3f0
Bohdan Partyk <info@podebrady.org>
2020-06-02 19:04:54 +0200

    update devstack

--- gulp/task/webpack.js
+++ gulp/task/webpack.js
@@ -1,5 +1,6 @@
 const isProduction = require('./../helpers/isProduction');
 const console = require('better-console');
+const process = require('process');
 
 // gulp dependencies
 const gulp = require('gulp');
@@ -12,5 +13,13 @@ const webpackProd = require('./../../webpack.prod');
 
 gulp.task('webpack', (callback) => {
     console.info('Webpack compile');
-    webpack(isProduction() ? webpackProd : webpackDev, () => callback());
+    webpack(isProduction() ? webpackProd : webpackDev, (error, stats) => {
+        const jsonStats = stats.toJson();
+        if (isProduction() && jsonStats.errors) {
+            console.error(jsonStats.errors.toString());
+            process.exit(1);
+        } else {
+            callback();
+        }
+    });
 });
